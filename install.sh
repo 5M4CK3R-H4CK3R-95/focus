73a921072a43e6ab521028c251fc8fd69571ba13
5M4CK3R-H4CK3R-95 <62463436+5M4CK3R-H4CK3R-95@users.noreply.github.com>
2020-03-21 09:15:32 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo 'Please Wait While Installing'
+sleep 2
+echo 'installing focus tool please Wait'
+sleep 3
+mv focus /usr/bin
+cd /usr/bin
+chmod +x focus
+rm -rf installe.sh
+echo 'Type focus In Your Windows Directory To Open Focus ....'
+sleep 3
